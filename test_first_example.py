e86dfe76156c74a88772551bfe68cb434cd48951
QA-Aidan <aidanaedy.qa@gmail.com>
2023-02-20 12:06:37 +0000

    changed files for first example as I broke the imports and so changed the files to now use classes and added unittest as well

--- test_first_example.py
+++ test_first_example.py
@@ -1,17 +1,18 @@
 import first_example
+import unittest
+class TestingFile(unittest.TestCase):
+
+    def test_answer1(self):
+        assert first_example.func(6) == 12
 
 
-def test_answer1():
-    assert first_example.func(6) == 12
+    def test_answer2(self):
+        assert first_example.func(5) == 10
 
 
-def test_answer2():
-    assert first_example.func(5) == 10
+    def test_answer3(self):
+        assert first_example.func(2) == 4
 
 
-def test_answer3():
-    assert first_example.func(2) == 4
-
-
-def test_answer4():
-    assert first_example.func(3) == 6
+    def test_answer4(self):
+        assert first_example.func(3) == 6
