9cc455748820c3138a9244c95415fde3dd7dc2e2
QA-Aidan <aidanaedy.qa@gmail.com>
2023-02-20 15:17:45 +0000

    creating some edge case tests and adding them with better names

--- test_first_example.py
+++ test_first_example.py
@@ -3,17 +3,22 @@ import unittest
 
 class TestingFile(unittest.TestCase):
 
-    def test_answer1(self):
-        assert first_example.func(6) == 12
-
-
-    def test_answer2(self):
+    def test_five_by_two_is_ten(self):
         assert first_example.func(5) == 10
 
 
-    def test_answer3(self):
+    def test_two_by_two_is_four(self):
         assert first_example.func(2) == 4
 
 
-    def test_answer4(self):
+    def test_three_by_two_is_six(self):
         assert first_example.func(3) == 6
+
+    def test_edges(self):
+        self.assertEqual(first_example.func(2), 2 * 2)
+        self.assertEqual(first_example.func(124), 124 * 2)
+        self.assertEqual(first_example.func(124), 2 * 124)
+        self.assertEqual(first_example.func(65535), 65535 * 2)
+        self.assertEqual(first_example.func(65535), 2 * 65535)
+        self.assertEqual(first_example.func(1), 1 * 2)
+        self.assertEqual(first_example.func(-1), (-1) * 2)
